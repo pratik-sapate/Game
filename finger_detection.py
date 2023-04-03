939bc41180f9e8bb63857f033a9462e1825e3a53
Pratik Vinod Sapate <pratik-sapate@users.noreply.github.com>
2023-04-03 10:09:09 +0530

    Update finger_detection.py

--- finger_detection.py
+++ finger_detection.py
@@ -5,8 +5,6 @@ import mediapipe as mp
 class HandDetection:
     def __init__(self):
         self.mp_hands = mp.solutions.hands
-        self.mp_drawing_styles = mp.solutions.drawing_styles
-        self.mp_drawing = mp.solutions.drawing_utils
         self.hands = self.mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.5)
 
     def get_point_finger_coordinate(self, image):
@@ -39,12 +37,6 @@ class HandDetection:
                     right_x = results[0].landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].x * width
                     right_y = results[0].landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].y * height
 
-            self.mp_drawing.draw_landmarks(
-                image,
-                results[0],
-                self.mp_hands.HAND_CONNECTIONS,
-                self.mp_drawing_styles.get_default_hand_landmarks_style(),
-                self.mp_drawing_styles.get_default_hand_connections_style())
             # print(len(results))
         if left_x != None and right_x == None:
             # left hand detected
@@ -58,14 +50,3 @@ class HandDetection:
         else:
             # No hand detected
             return None, None
-
-if __name__=="__main__":
-    cam = cv2.VideoCapture(0)
-    hand_capture = HandDetection()
-    while True:
-        ret, image = cam.read()
-        left, right = hand_capture.get_point_finger_coordinate(image)
-        print(left, right)
-        cv2.imshow('image', image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
